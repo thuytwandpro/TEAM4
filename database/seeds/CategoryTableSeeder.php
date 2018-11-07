070c3e72cdc0b847fda0be41726d1218ab0db886
thuytam <nguyenthuytam16@gmail.com>
2018-11-07 15:39:22 +0700

    update create and insert data #10

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -12,12 +12,12 @@ class CategoryTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
-            ['id' => 1, 'name' => 'Nike', 'slug' => ''],
-            ['id' => 2, 'name' => 'Sneakers', 'slug' => ''],
-            ['id' => 3, 'name' => 'Converses', 'slug' => ''],
-            ['id' => 4, 'name' => 'Adidas', 'slug' => ''],
-            ['id' => 5, 'name' => 'Vans', 'slug' => ''],
-            ['id' => 6, 'name' => 'Boots', 'slug' => ''],
+            ['id' => 1, 'name' => 'Nike', 'slug' => 'nike'],
+            ['id' => 2, 'name' => 'Sneakers', 'slug' => 'sneakers'],
+            ['id' => 3, 'name' => 'Converses', 'slug' => 'converses'],
+            ['id' => 4, 'name' => 'Adidas', 'slug' => 'adidas'],
+            ['id' => 5, 'name' => 'Vans', 'slug' => 'vans'],
+            ['id' => 6, 'name' => 'Boots', 'slug' => 'boots'],
         ]);
     }
 }
