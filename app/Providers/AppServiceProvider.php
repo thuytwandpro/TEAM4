10500e698cb46566052773cc215ef5aa4829aea1
Quang Rô <thuytwandpro@gmail.com>
2019-04-24 21:00:55 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this -> app -> bind('path.public', function()
+{
+        return base_path('public');
+});
     }
 }
